c5ba9a46ab590ca93d35087970e1eadb444487e3
Arun K <arthurbishop120@gmail.com>
2023-09-23 23:37:18 +0530

    minor changes in structure

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -1,11 +1,12 @@
 package domain
 
 type User struct {
-	ID       int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name     string `json:"name" gorm:"not null"`
-	Email    string `json:"email" gorm:"not null"`
-	Phone    string `json:"phone" gorm:"unique;not null"`
-	Password string `json:"password" gorm:"not null"`
+	ID        int    `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name      string `json:"name" gorm:"not null"`
+	Email     string `json:"email" gorm:"not null"`
+	Phone     string `json:"phone" gorm:"unique;not null"`
+	Password  string `json:"password" gorm:"not null"`
+	IsBlocked bool   `json:"is_blocked" gorm:"Default:false"`
 }
 
 type UserNotification struct {
